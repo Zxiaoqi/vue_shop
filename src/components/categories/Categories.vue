1db47a639993531ce83ee8e926b7ecde4c298b2b
zxiaoqi <mrzeng18229230299@163.com>
2020-01-29 15:01:07 +0800

    商品分类管理

--- src/components/categories/Categories.vue
+++ src/components/categories/Categories.vue
@@ -31,11 +31,11 @@
             v-else>三级</el-tag>
           </template>
           <!-- 操作 -->
-          <template slot='opt'>
+          <template slot='opt' slot-scope='scope'>
             <el-button type="primary" icon="el-icon-edit"
-            size="mini">编辑</el-button>
+            size="mini" @click="showEditDialog(scope.row.cat_id)">编辑</el-button>
             <el-button type="danger" icon="el-icon-delete"
-            size="mini">删除</el-button>
+            size="mini" @click="deleteCate(scope.row.cat_id)">删除</el-button>
           </template>
         </tree-table>
         <!-- 分页器 -->
@@ -51,9 +51,9 @@
         </el-pagination>
       </div>
     </el-card>
-    <!-- 添加分类 -->
+    <!-- 添加编辑分类 -->
     <el-dialog
-    title="添加分类"
+    :title="title"
     :visible.sync="addCatDialogVisible"
     width="40%" @close="clearSelect">
       <span>
@@ -64,19 +64,21 @@
           <el-form-item label="分类名称:" prop="cat_name">
             <el-input v-model="addCatForm.cat_name"></el-input>
           </el-form-item>
-          <el-form-item label="父级分类:">
-            <el-cascader
-            v-model="selectOptions"
-            :options="selectParentcate"
-            :props='selectProps'
-            @change="changeParentCate" clearable>
-            </el-cascader>
-          </el-form-item>
+          <template v-if="title==='添加分类'">
+            <el-form-item label="父级分类:">
+              <el-cascader
+              v-model="selectOptions"
+              :options="selectParentcate"
+              :props='selectProps'
+              @change="changeParentCate" clearable>
+              </el-cascader>
+            </el-form-item>
+          </template>
         </el-form>
       </span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addCatDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCatDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="selectAddEdit">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -109,7 +111,9 @@ export default {
       ],
       addCatDialogVisible: false,
       addCatForm: {
-        cat_name: ''
+        cat_name: '',
+        cat_level: 0,
+        cat_pid: 0
       },
       addCatFormRules: {
         cat_name: [{ required: true, message: '请输入分类名称', trigger: 'blur' }]
@@ -120,9 +124,10 @@ export default {
         expandTrigger: 'hover',
         value: 'cat_id',
         label: 'cat_name',
-        children: 'children',
-        checkStrictly: true
-      }
+        children: 'children'
+      },
+      title: '',
+      editId: 0
     }
   },
   methods: {
@@ -152,7 +157,95 @@ export default {
         })
     },
     // 父级分类改变
-    changeParentCate () {},
+    changeParentCate () {
+      if (this.selectOptions.length > 0) {
+        this.addCatForm.cat_pid = this.selectOptions[this.selectOptions.length - 1]
+        this.addCatForm.cat_level = this.selectOptions.length
+      } else {
+        this.addCatForm.cat_pid = 0
+        this.addCatForm.cat_level = 0
+      }
+    },
+    // 添加分类
+    addCate () {
+      this.$refs.addCatFormRef.validate(valid => {
+        if (!valid) return false
+        this.$http.post('categories',
+          this.addCatForm)
+          .then(res => {
+            const { meta } = res.data
+            if (meta.status === 201) {
+              this.$message.success(meta.msg)
+              this.addCatDialogVisible = false
+              this.getCategoriesData()
+              this.getParentCateList()
+            } else {
+              this.$message.error(meta.msg)
+            }
+          })
+      })
+    },
+    // 搜索-编辑分类
+    showEditDialog (id) {
+      this.title = '编辑分类'
+      this.addCatDialogVisible = true
+      this.$http.get(`categories/${id}`).then(res => {
+        const { data, meta } = res.data
+        if (meta.status === 200) {
+          this.addCatForm = data
+          this.editId = id
+        } else {
+          this.$message.error(meta.msg)
+        }
+      })
+    },
+    editCate () {
+      this.$refs.addCatFormRef.validate(valid => {
+        if (!valid) return false
+        this.$http.put(`categories/${this.editId}`,
+          this.addCatForm).then(res => {
+          const { meta } = res.data
+          if (meta.status === 200) {
+            this.addCatDialogVisible = false
+            this.getCategoriesData()
+            this.$message.success(meta.msg)
+          } else {
+            this.$message.error(meta.msg)
+          }
+        })
+      })
+    },
+    // 判断是添加还是编辑
+    selectAddEdit () {
+      if (this.title === '添加分类') {
+        this.addCate()
+      } else if (this.title === '编辑分类') {
+        this.editCate()
+      }
+    },
+    // 删除分类
+    deleteCate (id) {
+      this.$confirm('此操作将永久删除该分类, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http.delete(`categories/${id}`).then(res => {
+          const { meta } = res.data
+          if (meta.status === 200) {
+            this.$message.success(meta.msg)
+            this.getCategoriesData()
+          } else {
+            this.$message.error(meta.msg)
+          }
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
+    },
     // 显示页数
     handleSizeChange (val) {
       this.queryinfo.pagesize = val
@@ -163,8 +256,14 @@ export default {
       this.queryinfo.pagenum = val
       this.getCategoriesData()
     },
+    // 监听对话框关闭事件，重置表单数据
     clearSelect () {
-      this.addCatForm = {}
+      this.$refs.addCatFormRef.resetFields()
+      this.selectOptions = []
+      if (this.title === '添加分类') {
+        this.addCatForm.cat_level = 0
+        this.addCatForm.cat_pid = 0
+      }
     }
   },
   created () {
